d6b84ce34563597878ee238c0224762aaca5a4f5
Hooman Hasanzadeh <mr.subscript@gmail.com>
2020-11-26 21:10:14 +0330

    Added FBI attack weapons

--- src/static/siege/weapons.ts
+++ src/static/siege/weapons.ts
@@ -1,14 +1,26 @@
 export enum Primary {
+  // SAS
 	L85A2 = 'l85a2',
 	M590A1 = 'm590a1',
   AR33 = 'ar33',
   FMG_9 = 'fmg-9',
   MP5K = 'mp5k',
+
+  // FBI
+  G36C = 'g36c',
+  R4_C = 'r4-c',
+  M1014 = 'm1014',
+  _556XI = '556xi'
 }
 
 export enum Secondary {
+  // SAS
 	P226_MK_25 = 'p226-mk-25',
-	SMG_11 = 'smg-11',
+  SMG_11 = 'smg-11',
+  
+  // FBI
+  _5_7_USG = '5.7-usg',
+  M45_MEUSOC = 'm45-meusoc',
 }
 
 // TODO: create weapons class
